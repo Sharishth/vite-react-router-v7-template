a5cbae32d6c83e4f97097624f8d2c3988ff37e35
Sharishth Singh <44324361+Sharishth@users.noreply.github.com>
2025-01-23 10:58:27 +0530

    Added Template files

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function About() {
+  return (
+    <>
+      <p>This is General React Template with react-router-dom v7.</p>
+      <Link to='/'>Home</Link>
+    </>
+  )
+}
